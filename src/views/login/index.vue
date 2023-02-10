c39ff19a6875871ba4458d93809b5ed2378debe3
yanguodong <563985053@qq.com>
2023-02-10 17:11:04 +0800

    登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>123</div>
+  <div class="box">
+    <div class="inp"></div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,4 +15,23 @@ const bbb = ref('')
 onMounted(() => {})
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.box {
+  width: 100vw;
+  height: 100vh;
+  background: url(@/assets/image/loginback.png) repeat center;
+  background-size: 100%;
+  // opacity: 0.5;
+  .inp {
+    width: 25vw;
+    height: 30vw;
+    background: skyblue;
+    opacity: 0.5;
+    position: absolute;
+    top: 50%;
+    left: 80%;
+    transform: translate(-50%, -50%);
+    box-shadow: 0px 0px 15px black;
+  }
+}
+</style>
